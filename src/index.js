6eb45b280e58fb430a37a6d1d0c46ed28f14ceac
Sabrina <ssamuelrome@gmail.com>
2021-02-21 01:33:37 +0000

    Updated time and weather icon

--- src/index.js
+++ src/index.js
@@ -1,46 +1,60 @@
-//Display date and time
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-
-let day = days[now.getDay()];
-
 function changeToDoubleDigits(time) {
   if (time < 10) {
-    return "0" + time;
+    return `0${time}`;
   } else {
     return time;
   }
 }
 
-let hour = now.getHours();
-let minutes = now.getMinutes();
+//Display date and time
+function formatTime(timestamp) {
+  let now = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
 
-let currentDateTime = `${day} ${changeToDoubleDigits(
-  hour
-)}:${changeToDoubleDigits(minutes)}`;
+  return `${day} ${changeToDoubleDigits(hour)}:${changeToDoubleDigits(
+    minutes
+  )}`;
+}
+// let hour = now.getHours();
+// let minutes = now.getMinutes();
 
-let time = document.querySelector("#time");
-time.innerHTML = currentDateTime;
+// let currentDateTime = `${day} ${changeToDoubleDigits(
+//   hour
+// )}:${changeToDoubleDigits(minutes)}`;
+
+// let time = document.querySelector("#time");
+// time.innerHTML = currentDateTime;
 
 //get weather for submitted city
 function displayCityTemperature(response) {
   let city = document.querySelector("#city");
-  let todaysTemp = document.querySelector(".todays-temp");
   let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let precipitation = document.querySelector("#precipitation");
+  let time = document.querySelector("#time");
+  let todaysTemp = document.querySelector(".todays-temp");
+  let todaysWeatherIcons = document.querySelector("#todays-icon");
   let windSpeed = document.querySelector("#wind-speed");
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = `${response.data.main.humidity}`;
+  precipitation.innerHTML = `?`;
+  time.innerHTML = formatTime(response.data.dt + response.data.timezone);
   todaysTemp.innerHTML = `${Math.round(response.data.main.temp)}°C`;
-  windSpeed.innerHTML = `Wind speed: ${response.data.wind.speed}m/sec`;
+  todaysWeatherIcons.innerHTML = `<img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"/>`;
+  windSpeed.innerHTML = `${Math.round(response.data.wind.speed)} mps`;
 }
 
 function getCityTemperature(event) {
@@ -89,15 +103,15 @@ function getDefaultCity() {
 getDefaultCity();
 
 //Convert temperature
-// function changeUnit(event) {
-//   event.preventDefault();
-//   let todaysTemp = document.querySelector(".todays-temp");
-//   if (event.target.id === "fahrenheit") {
-//     todaysTemp.innerHTML = "57°";
-//   } else {
-//     todaysTemp.innerHTML = "14°";
-//   }
-// }
+function changeUnit(event) {
+  event.preventDefault();
+  let todaysTemp = document.querySelector(".todays-temp");
+  if (event.target.id === "fahrenheit") {
+    todaysTemp.innerHTML = "57°";
+  } else {
+    todaysTemp.innerHTML = "14°";
+  }
+}
 
 // let changeToCelsius = document.querySelector("#celsius");
 // changeToCelsius.addEventListener("click", changeUnit);
@@ -105,4 +119,11 @@ getDefaultCity();
 // let changeToFahrenheit = document.querySelector("#fahrenheit");
 // changeToFahrenheit.addEventListener("click", changeUnit);
 
-//navigator.geoLocation.getCurrentPosition
+let changeToFahrenheit = document.querySelector("button.fahrenheit");
+changeToFahrenheit.addEventListener("click", changeUnit);
+
+{
+  /* <button type="button" class="btn btn-primary mb-3 fahrenheit">
+  Change to Fahrenheit
+</button>; */
+}
